01ae0ea594fc7d613139547963b5ef6719b6eba7
i.ivanov <i.ivanov@fcorp.top>
2021-09-25 09:46:37 +0800

    first commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="favorite">
+        <img src="/img/like-off.svg" alt="Unlike"/>
+      </div>
+      <img width={133} height={112} src={props.img} alt=""/>
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена: </span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button" onClick={() => console.log('Нажали на плюс')}>
+          <img src="/img/plus-off.svg" alt="Unlike"/>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
